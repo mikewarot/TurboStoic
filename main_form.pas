623cb232da9854d288fee62b1cea14980cc1b837
Mike Warot <chezmike@gmail.com>
2026-05-27 12:50:25 -0500

    Add initial form input and status areas

--- main_form.pas
+++ main_form.pas
@@ -5,18 +5,22 @@ unit main_form;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, StdCtrls;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
     MainMenu1: TMainMenu;
+    Memo1: TMemo;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
     MenuItem4: TMenuItem;
+    procedure Button1Click(Sender: TObject);
     procedure MenuItem3Click(Sender: TObject);
     procedure MenuItem4Click(Sender: TObject);
   private
@@ -42,6 +46,11 @@ end;
 procedure TForm1.MenuItem3Click(Sender: TObject);
 begin
   ShowMessage('TurboStoic version 0.01');
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Memo1.Append(Edit1.Text);
 end;
 
 end.
